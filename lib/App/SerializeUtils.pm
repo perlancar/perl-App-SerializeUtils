3d784e35ef5716fb299a32d176451801c9e37273
Steven Haryanto (on PC, Bandung) <stevenharyanto@gmail.com>
2014-05-22 15:58:07 +0700

    Symlink convert scripts instead of copy, support Sereal and Storable formats

--- lib/App/SerializeUtils.pm
+++ lib/App/SerializeUtils.pm
@@ -23,19 +23,39 @@ and output to stdout):
 
  dd2json
  dd2phpser
+ dd2sereal
+ dd2storable
  dd2yaml
 
  json2dd
  json2phpser
+ json2sereal
+ json2storable
  json2yaml
 
  phpser2dd
  phpser2json
+ phpser2sereal
+ phpser2storable
  phpser2yaml
+
+ sereal2dd
+ sereal2json
+ sereal2phpser
+ sereal2storable
+ sereal2yaml
+
+ storable2dd
+ storable2json
+ storable2phpser
+ storable2sereal
+ storable2yaml
 
  yaml2dd
  yaml2json
  yaml2phpser
+ yaml2sereal
+ yaml2storable
 
 Also included is this utility to pretty-print JSON:
 
@@ -55,6 +75,10 @@ L<Data::Dump>
 L<JSON>
 
 L<PHP::Serialization>
+
+L<Sereal>
+
+L<Storable>
 
 L<YAML>
 
