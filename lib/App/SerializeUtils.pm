3cc8390f5554c80bf045ed1dbf8bb96b0a844ed7
perlancar (@pc-home) <perlancar@gmail.com>
2015-04-04 22:40:42 +0700

    [doc] Use DZP:InsertExecsList to replace manual list of scripts in Description

--- lib/App/SerializeUtils.pm
+++ lib/App/SerializeUtils.pm
@@ -16,56 +16,10 @@ use warnings;
 
 =head1 DESCRIPTION
 
-This distributions provides the following command-line utilities:
+This distributions provides the following command-line utilities related to
+serialization:
 
-For converting between serializable formats (each of these take input from stdin
-and output to stdout):
-
- dd2json
- dd2phpser
- dd2sereal
- dd2storable
- dd2yaml
-
- json2dd
- json2phpser
- json2sereal
- json2storable
- json2yaml
-
- phpser2dd
- phpser2json
- phpser2sereal
- phpser2storable
- phpser2yaml
-
- sereal2dd
- sereal2json
- sereal2phpser
- sereal2storable
- sereal2yaml
-
- storable2dd
- storable2json
- storable2phpser
- storable2sereal
- storable2yaml
-
- yaml2dd
- yaml2json
- yaml2phpser
- yaml2sereal
- yaml2storable
-
-Also included is this utility to pretty-print JSON:
-
- pp-json
-
-And utilities for checking:
-
- check-json
- check-phpser
- check-yaml
+# INSERT_EXECS_LIST
 
 
 =head1 SEE ALSO
